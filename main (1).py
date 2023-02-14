6b2d14dbc31e140f08d18bb321d3f8da54547177
Alina1616 <117991271+Alina1616@users.noreply.github.com>
2023-02-14 10:02:27 +0300

    №2 14.02.2023

---
+++ main (1).py
@@ -0,0 +1,16 @@
+#1
+a=float(input())
+b=float(input())
+
+#2
+if (a > b) and (a-b < b):
+    print("True")
+else:
+    print("False")
+
+#3
+if (a > b) or (a-b < b):
+    print("True")
+else:
+    print("False")
+    
